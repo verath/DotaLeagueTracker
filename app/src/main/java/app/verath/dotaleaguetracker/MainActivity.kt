0153387f8e30fe86cb15ae73030e0df189113f01
Peter Eliasson <warackta@gmail.com>
2018-05-11 08:49:49 +0200

    Initial commit

---
+++ app/src/main/java/app/verath/dotaleaguetracker/MainActivity.kt
@@ -0,0 +1,15 @@
+package app.verath.dotaleaguetracker
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+}
