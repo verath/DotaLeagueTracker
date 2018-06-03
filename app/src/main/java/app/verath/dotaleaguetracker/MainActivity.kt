764182e6124af9106778a6ef3bfe6b396fb26934
Peter Eliasson <warackta@gmail.com>
2018-06-03 11:11:33 +0200

    Hide drawer on back press

--- app/src/main/java/app/verath/dotaleaguetracker/MainActivity.kt
+++ app/src/main/java/app/verath/dotaleaguetracker/MainActivity.kt
@@ -1,14 +1,24 @@
 package app.verath.dotaleaguetracker
 
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerVisible(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
